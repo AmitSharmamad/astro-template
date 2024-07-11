f3be6275db3f27f5b11eda503d12ef693e62c93b
Madgula Amit <amitsharmamad@gmail.com>
2024-07-11 23:46:15 +0530

    added more plugins

--- lua/community.lua
+++ lua/community.lua
@@ -12,7 +12,14 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.sql" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.xml" },
   { import = "astrocommunity.pack.docker" },
+
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.test.nvim-coverage" },
+
+  { import = "astrocommunity.utility.noice-nvim" },
 }
